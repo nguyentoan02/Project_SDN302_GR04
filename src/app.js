9baef55f87d7cdce1b4b6efdf7543404e57ad90e
= <=cinepa.org@gmail.com>
2025-01-20 21:59:43 +0700

    update config source

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
-const express = require("express");
-const { connectToDatabase } = require("./config/database");
-const configureExpress = require("./config/express");
+const connectToDatabase = require('./config/database');
+const app = require('./config/express');
 
-const app = configureExpress();
 const PORT = process.env.PORT || 3000;
 
 connectToDatabase();
--- src/config/express.js
+++ src/config/express.js
@@ -1,13 +1,35 @@
+require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+const { successHandler } = require('../core/success');
+const { handleError } = require('../core/error');
+const router = require('../routes/api');
 const app = express();
 
-// Middleware configuration
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(compression());
+app.use(morgan('dev'));
 
-// Export the configured Express app
+// Success handler middleware
+app.use(successHandler);
+
+// Routes
+app.use('/', router);
+
+// Error handling
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use(handleError);
+
 module.exports = app;
