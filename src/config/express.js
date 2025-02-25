ce607b7fd8752b5dbef0d5b3a3b9748f15696602
GitF1 <169064951+GitF1@users.noreply.github.com>
2025-02-25 21:53:12 +0700

    Feature/config (#9)
    * feat(cart): implement cart services
    
    * feat(core): add BadRequest
    
    * feat(route): config dynamic router
    
    * feat(router): update config router app
    
    * feat(cart): update router async cart
    
    * feat(router): update handle wrapper async api
    
    * feat(route): add 404 router response
    
    * feat(config): config view view engine ejs
    
    * feat(views): add base  view pages
    
    ---------
    
    Co-authored-by: = <=cinepa.org@gmail.com>

--- src/config/express.js
+++ src/config/express.js
@@ -4,20 +4,29 @@ const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const compression = require('compression');
+const path = require('path');
 const { successHandler } = require('../core/success');
 const { handleError } = require('../core/error');
 const bodyParser = require('body-parser');
 const router = require('../routes');
 
 const app = express();
-
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+// Serve static files
+app.use(express.static(path.join(__dirname, '../public')));
 // Middleware
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false // For development only
+  })
+);
 app.use(compression());
 app.use(morgan('dev'));
 
@@ -26,12 +35,14 @@ app.use(successHandler);
 
 // Routes
 app.use('/api', router);
+app.use('/', require('../routes/views/view.routes'));
 
 // 404 Handler
 app.all('*', (req, res, next) => {
-  const err = new Error(`Cannot find ${req.originalUrl} on this server!`);
-  err.status = 404;
-  next(err);
+  res.status(404).render('404', {
+    title: '404 Not Found',
+    message: `Cannot find ${req.originalUrl} on this server!`
+  });
 });
 
 // Error handling
