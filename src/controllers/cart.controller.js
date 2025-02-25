a98fbe1a6336c03b8a2bc38f118bde7494f69b88
= <=cinepa.org@gmail.com>
2025-02-25 22:57:02 +0700

    feat(cart): update cart services

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,13 +1,16 @@
-const User = require('../models/user');
+const mongoose = require('mongoose');
 const { SuccessResponse } = require('../core/success');
 const { NotFoundError, DatabaseError, BadRequest } = require('../core/error');
+
+const User = require('../models/user');
+const Product = require('../models/Product');
 
 class CartController {
   async getCart(req, res) {
     if (!req?.user?._id) {
       throw new BadRequest('Missing Param');
     }
-    const carts = await User.findById(req.user._id).select('cart').populate('cart.product');
+    const carts = await User.findById(_id).select('cart');
     return SuccessResponse.ok(res, carts, 'OK!');
   }
 
@@ -21,29 +24,54 @@ class CartController {
       throw new BadRequest('Quantity must be greater than 0!');
     }
 
-    if ((await Article.findById(product_id)) === null) {
+    if ((await Product.findById(product_id)) === null) {
       throw new NotFoundError('Product not found!');
     }
 
-    const user = await User.findByIdAndUpdate(
-      req.user._id,
-      {
-        $push: {
-          cart: {
-            product: product_id,
-            quantity: quantity
+    const _id = req.user._id;
+
+    const existingCart = await User.findOne({
+      _id,
+      'cart.product': product_id
+    });
+
+    let result;
+
+    if (existingCart) {
+      result = await User.findOneAndUpdate(
+        {
+          _id,
+          'cart.product': product_id
+        },
+        {
+          $inc: {
+            'cart.$.quantity': quantity
           }
+        },
+        { new: true }
+      );
+    } else {
+      result = await User.findByIdAndUpdate(
+        _id,
+        {
+          $push: {
+            cart: {
+              product: product_id,
+              quantity: quantity
+            }
+          }
+        },
+        {
+          new: true
         }
-      },
-      {
-        new: true
-      }
-    );
-    if (!user) {
+      );
+    }
+
+    if (!result) {
       throw new DatabaseError('Something Error!');
     }
 
-    return SuccessResponse.created(res, user, 'Add to cart successfully!');
+    return SuccessResponse.created(res, { product_id }, 'Add to cart successfully!');
   }
 
   async updateAmount(req, res) {
@@ -52,24 +80,28 @@ class CartController {
     if (!product_id || !quantity) {
       throw new BadRequest('Missing param!');
     }
-    if (quantity <= 0) {
-      throw new BadRequest('Quantity must be greater than 0!');
+
+    const quantityUpdate = parseInt(quantity);
+
+    if (isNaN(quantityUpdate)) {
+      throw new BadRequest('Quantity must be a  number!');
     }
 
-    let quantityUpdate = parseInt(quantity);
+    const _id = req.user._id;
 
     const result = await User.findOneAndUpdate(
       {
-        _id: req.user._id,
+        _id,
         'cart.product': product_id
       },
       {
-        $set: {
-          'cart.$.quantity': 'cart.$.quantity' + quantityUpdate
+        $inc: {
+          'cart.$.quantity': quantityUpdate
         }
       },
       {
-        new: true
+        new: true,
+        runValidators: true
       }
     );
 
@@ -77,7 +109,7 @@ class CartController {
       throw new DatabaseError('Something Error!');
     }
 
-    return SuccessResponse.ok(res, result, 'Update amount successfully!');
+    return SuccessResponse.ok(res, result.cart, 'Update amount successfully!');
   }
 
   async removeItem(req, res) {
