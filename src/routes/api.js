dc5fe98ef2a5b56780bb51f9a8c7b079d7372290
GitF1 <169064951+GitF1@users.noreply.github.com>
2025-02-25 23:01:37 +0700

    feat(cart): implement cart services (#7)
    * feat(cart): implement cart services
    
    * feat(cart): update router async cart
    
    * feat(cart): update cart services
    
    ---------
    
    Co-authored-by: = <=cinepa.org@gmail.com>

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,11 +3,16 @@ const router = express.Router();
 const globalAsyncHandler = require('../middleware/handler');
 const { getRoutes } = require('./register.routes');
 
+//import router
+require('./cart/cart.routes');
+
 // Apply global async handler to router
 globalAsyncHandler(router);
 
+// router.use('/cart', routerCart);
+
 // Register all routes from the registry
-getRoutes().forEach(({ path, router: moduleRouter }) => {
+getRoutes()?.forEach(({ path, router: moduleRouter }) => {
   if (!path || typeof path !== 'string') {
     throw new Error(`Invalid route path: ${path}`);
   }
@@ -17,6 +22,7 @@ getRoutes().forEach(({ path, router: moduleRouter }) => {
   }
 
   const normalizedPath = path.startsWith('/') ? path : `/${path}`;
+  console.log(normalizedPath, '\n');
 
   router.use(normalizedPath, moduleRouter);
 });
