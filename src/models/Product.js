5d2b23fe201a521bf8868cbbd110099f67a94c7d
theycallmeVinh <20072003vinh@gmail.com>
2025-03-03 13:15:04 +0700

    feat(model): update attributes model Product

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,11 +1,31 @@
-// models/Product.js
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
 const ProductSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    price: Number,
-    stock: Number,
-    category: String,
-    image: String, // URL của ảnh sản phẩm
-    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }]
-  });
-  module.exports = mongoose.model('Product', ProductSchema);
+  name: { type: String, required: true },
+  code: { type: String, required: true, unique: true },
+  author: { type: String, required: true },
+  category: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true, min: 0 },
+  stock: { type: Number, required: true, min: 0 },
+  image: { type: String, required: true },
+  publisher: { type: String, required: true },
+  publishYear: { type: Number, required: true },
+  pages: { type: Number, min: 1, required: true },
+  dimensions: { type: String, required: true },
+  country: { type: String, required: true },
+  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+  rating: { type: Number, default: 0, min: 0, max: 5 },
+  createdAt: { type: Date, default: Date.now },
+  slug: { type: String, unique: true }
+});
+
+ProductSchema.pre('save', function (next) {
+  if (this.isModified('name')) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
+  }
+  next();
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
