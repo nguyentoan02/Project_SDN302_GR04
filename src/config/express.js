7d26d978ac59ccef6571d2dca0a176f3f25b4e07
= <=cinepa.org@gmail.com>
2025-03-24 18:03:52 +0700

    feat(core): re-config for helmet to handle script js

--- src/config/express.js
+++ src/config/express.js
@@ -12,9 +12,8 @@ const MongoStore = require('connect-mongo');
 const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override');
 const helper = require('../utils/helper');
-const flash = require('connect-flash');
-const jwt = require('jsonwebtoken');
 const session = require('express-session');
+const { authStoreLocalUser } = require('../middleware/auth');
 
 const app = express();
 
@@ -31,21 +30,7 @@ app.use(express.static(path.join(__dirname, '../../public')));
 
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  const token = req.cookies.token;
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET || 'your-secure-secret-key');
-      req.user = decoded;
-    } catch (err) {
-      req.user = null;
-    }
-  } else {
-    req.user = null;
-  }
-  res.locals.user = req.user; // Gán vào res.locals để dùng trong template
-  next();
-});
+app.use(authStoreLocalUser);
 
 app.use(
   session({
@@ -69,9 +54,15 @@ app.use(cors());
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      'default-src': ["'self'"],
-      'img-src': ['*'],
-      'script-src': ["'self'", "'unsafe-inline'", 'https://cdn.jsdelivr.net']
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"], // Allow inline scripts
+      styleSrc: ["'self'", "'unsafe-inline'"], // Allow inline styles
+      imgSrc: ["'self'", 'data:', 'https:'],
+      connectSrc: ["'self'"],
+      fontSrc: ["'self'"],
+      objectSrc: ["'none'"],
+      mediaSrc: ["'self'"],
+      frameSrc: ["'none'"]
     },
     contentSecurityPolicy: false
   })
